bd8fc008eabb40a04bbe73db4bab26ea0caadcca
Ventona <defreitas.tony.pro@gmail.com>
2021-02-08 14:02:47 +0100

    update v2

--- src/banque/Client.java
+++ src/banque/Client.java
@@ -6,9 +6,14 @@ import java.util.Date;
 public class Client {
     private String nom;
     private int nbComptes;
+    private Date dateDeNaissance;
 
     public Client(String nom, Date dateDeNaissance){
         this.nom = nom;
         this.dateDeNaissance = dateDeNaissance;
+        Compte [] comptes = new Compte[100];
     }
+
+    public void
+
 }
--- src/banque/Compte.java
+++ src/banque/Compte.java
@@ -1,12 +1,11 @@
 package banque;
 
 public class Compte {
-    private float solde;
+    private float solde = 0;
 
     public Compte(float solde){
         this.solde = solde;
     }
-    
 
     public float getSolde(){
         return solde;
@@ -26,6 +25,7 @@ public class Compte {
 
     public void virer(float valeur, Compte destinataire){
         retrait(valeur);
+        destinataire.depot(valeur);
     }
 
 
