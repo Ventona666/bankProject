ff1d09b3d5dda29ad09c06ebef2df84dc17e86e3
Ventona <defreitas.tony.pro@gmail.com>
2021-02-08 13:44:38 +0100

    update v1

--- src/banque/Compte.java
+++ src/banque/Compte.java
@@ -3,26 +3,29 @@ package banque;
 public class Compte {
     private float solde;
 
+    public Compte(float solde){
+        this.solde = solde;
+    }
     
 
     public float getSolde(){
-
+        return solde;
     }
 
     public void depot(float valeur){
-
+        solde += valeur;
     }
 
     public void retrait(float valeur){
-
+        solde -= valeur;
     }
 
     public void afficherSolde(){
-
+        System.out.println("Solde du compte : " + solde + " €.");
     }
 
     public void virer(float valeur, Compte destinataire){
-
+        retrait(valeur);
     }
 
 
